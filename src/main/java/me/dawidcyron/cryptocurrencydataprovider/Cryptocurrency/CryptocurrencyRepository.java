90190135712c5f2b9648e9a2a9bf0a4cae14fc52
Dawid Cyron <dawidcyron@op.pl>
2019-04-23 10:58:24 +0200

    Configure Reposiotry for Cryptocurrency

--- src/main/java/me/dawidcyron/cryptocurrencydataprovider/Cryptocurrency/CryptocurrencyRepository.java
+++ src/main/java/me/dawidcyron/cryptocurrencydataprovider/Cryptocurrency/CryptocurrencyRepository.java
@@ -1,3 +1,7 @@
 package me.dawidcyron.cryptocurrencydataprovider.Cryptocurrency;
 
-public class CryptocurrencyRepository {}
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CryptocurrencyRepository extends CrudRepository<Cryptocurrency, String> {}
