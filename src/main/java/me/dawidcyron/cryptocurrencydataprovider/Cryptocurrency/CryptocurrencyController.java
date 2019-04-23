eb24b7b50b6ce4089162c71102bd4c1ac5ca4033
Dawid Cyron <dawidcyron@op.pl>
2019-04-23 13:43:05 +0200

    Add parameter handling

--- src/main/java/me/dawidcyron/cryptocurrencydataprovider/Cryptocurrency/CryptocurrencyController.java
+++ src/main/java/me/dawidcyron/cryptocurrencydataprovider/Cryptocurrency/CryptocurrencyController.java
@@ -4,14 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.List;
 
 @RestController
 public class CryptocurrencyController {
   @Autowired CryptocurrencyRepository cryptocurrencyRepository;
 
-  @GetMapping("/")
-  public ResponseEntity<Iterable<Cryptocurrency>> getCrypots() {
-    return new ResponseEntity<>(cryptocurrencyRepository.findAll(), HttpStatus.OK);
+  @GetMapping("/api")
+  public ResponseEntity getSelectedCryptocurrencies(
+      @RequestParam(value = "name", required = false) HashSet<String> cryptoList) {
+    if (cryptoList == null) {
+      return ResponseEntity.status(HttpStatus.OK).body(cryptocurrencyRepository.findAll());
+    } else {
+      if (cryptoList.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No value passed with parameter!");
+      }
+      return ResponseEntity.status(HttpStatus.OK).body(cryptocurrencyRepository.findAllById(cryptoList));
+    }
   }
 }
