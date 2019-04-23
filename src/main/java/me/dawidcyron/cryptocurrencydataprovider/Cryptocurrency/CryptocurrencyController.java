02007ac173ff06c19d0be94bca28ccc6502095a2
Dawid Cyron <dawidcyron@op.pl>
2019-04-23 11:00:44 +0200

    Add controller route to receive all coins

--- src/main/java/me/dawidcyron/cryptocurrencydataprovider/Cryptocurrency/CryptocurrencyController.java
+++ src/main/java/me/dawidcyron/cryptocurrencydataprovider/Cryptocurrency/CryptocurrencyController.java
@@ -1,3 +1,17 @@
 package me.dawidcyron.cryptocurrencydataprovider.Cryptocurrency;
 
-public class CryptocurrencyController {}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CryptocurrencyController {
+  @Autowired CryptocurrencyRepository cryptocurrencyRepository;
+
+  @GetMapping("/")
+  public ResponseEntity<Iterable<Cryptocurrency>> getCrypots() {
+    return new ResponseEntity<>(cryptocurrencyRepository.findAll(), HttpStatus.OK);
+  }
+}
