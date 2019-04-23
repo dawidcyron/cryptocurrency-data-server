0ba0085c65447f0537cbc223697adef7c3559946
Dawid Cyron <dawidcyron@op.pl>
2019-04-23 10:43:41 +0200

    Change Redis driver to Lettuce, enable scheduling

--- src/main/java/me/dawidcyron/cryptocurrencydataprovider/Configuration/Config.java
+++ src/main/java/me/dawidcyron/cryptocurrencydataprovider/Configuration/Config.java
@@ -3,21 +3,23 @@ package me.dawidcyron.cryptocurrencydataprovider.Configuration;
 import me.dawidcyron.cryptocurrencydataprovider.Cryptocurrency.Cryptocurrency;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 public class Config {
 
   @Bean
-  JedisConnectionFactory jedisConnectionFactory() {
-    return new JedisConnectionFactory();
+  LettuceConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory();
   }
 
   @Bean
   public RedisTemplate<String, Cryptocurrency> redisTemplate() {
     RedisTemplate<String, Cryptocurrency> template = new RedisTemplate<>();
-    template.setConnectionFactory(jedisConnectionFactory());
+    template.setConnectionFactory(redisConnectionFactory());
     return template;
   }
 }
